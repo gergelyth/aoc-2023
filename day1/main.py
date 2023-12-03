51b96ff18e641601e81431352abb0bdcaa3d5cff
gergelyth <gergelytothdev@gmail.com>
2023-12-03 19:25:48 +0000

    Day 1 - Part 1 done

--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,16 @@
 from aocd.models import Puzzle
+from core import test_and_submit
+from util import *
+
+def solution(input: str) -> tuple[any, any]:
+    lines = get_lines(input)
+    sum = 0
+    for line in lines:
+        first_digit = next(ch for ch in line if ch.isdigit())
+        last_digit = next(ch for ch in reversed(line) if ch.isdigit())
+        sum += int(f"{first_digit}{last_digit}")
+    
+    return (sum, None)
 
 puzzle = Puzzle(2023, 1)
-print(puzzle)
-print(puzzle.examples)
+test_and_submit(puzzle, solution, False)
