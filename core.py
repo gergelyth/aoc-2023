9ed7a91d90cc786e92efb50136a05c3fd2ff01cf
gergelyth <gergelytothdev@gmail.com>
2023-12-03 20:16:13 +0000

    Day 1 - Part 2 done

--- core.py
+++ core.py
@@ -3,11 +3,12 @@ from typing import Callable
 
 # As arguments, we take the puzzle and the algorithm which computes answer A and answer B.
 def test_and_submit(puzzle: Puzzle, algorithm: Callable[[str], tuple[any, any]], dry_run: bool = True):
-    for example in puzzle.examples:
+    examples = puzzle.examples[len(puzzle.examples)//2:] if puzzle.answered_a else puzzle.examples
+    for example in examples:
         test_result = algorithm(example.input_data)
-        if str(test_result[0]) != str(example.answer_a):
+        if example.answer_a and str(test_result[0]) != str(example.answer_a):
             raise AssertionError(f"Test A failed. Expected result: {example.answer_a}, got: {test_result[0]}")
-        if str(test_result[1]) != str(example.answer_b):
+        if example.answer_b and str(test_result[1]) != str(example.answer_b):
             raise AssertionError(f"Test B failed. Expected result: {example.answer_b}, got: {test_result[1]}")
         
     print("All primary tests passed.")
--- day1/main.py
+++ day1/main.py
@@ -1,16 +1,43 @@
 from aocd.models import Puzzle
 from core import test_and_submit
-from util import *
+from util import get_lines
+
+digit_words = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+def get_included_digit_word(part: str) -> int | None:
+    digits = [key for key, value in digit_words.items() if key in part]
+    return digits[0] if len(digits) > 0 else None
+
+def find_first_digit(line: str, is_reversed: bool) -> str:
+    cumm_window = ""
+    for ch in reversed(line) if is_reversed else line:
+        if ch.isdigit():
+            return ch
+
+        cumm_window = f"{ch}{cumm_window}" if is_reversed else f"{cumm_window}{ch}"
+        found_word = get_included_digit_word(cumm_window)
+        if found_word:
+            return digit_words[found_word]
 
 def solution(input: str) -> tuple[any, any]:
     lines = get_lines(input)
     sum = 0
     for line in lines:
-        first_digit = next(ch for ch in line if ch.isdigit())
-        last_digit = next(ch for ch in reversed(line) if ch.isdigit())
+        first_digit = find_first_digit(line, False)
+        last_digit = find_first_digit(line, True)
         sum += int(f"{first_digit}{last_digit}")
-    
-    return (sum, None)
+
+    return (sum, sum)
 
 puzzle = Puzzle(2023, 1)
 test_and_submit(puzzle, solution, False)
