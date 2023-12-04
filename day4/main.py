ca45809b375797f18bd029380b2fb1db0e4267a8
gergelyth <gergelytothdev@gmail.com>
2023-12-04 19:35:07 +0000

    Day 4 - Part 2 done

--- day4/main.py
+++ day4/main.py
@@ -6,15 +6,21 @@ from util import get_lines
 
 def solution(input: str) -> tuple[any, any]:
     lines = get_lines(input)
-    result_sum = 0
-    for line in lines:
+    points_total = 0
+    copies_tracker = dict((i, 1) for i in range(0, len(lines)))
+    for line_number, line in enumerate(lines):
         match = re.match(r".*: (.*) \| (.*)", line)
         winning_numbers = set(match.group(1).strip().split(" "))
         owned_numbers = set(match.group(2).strip().split(" "))
         matching_numbers = list(filter(None, winning_numbers.intersection(owned_numbers)))
-        result_sum += 2 ** (len(matching_numbers)-1) if len(matching_numbers) > 0 else 0
+        current_points = 2 ** (len(matching_numbers)-1) if len(matching_numbers) > 0 else 0
+        points_total += current_points
+        
+        for i in range(line_number+1, line_number+len(matching_numbers)+1):
+            copies_tracker[i] += copies_tracker[line_number]
 
-    return (result_sum, None)
+    total_copies = sum(copies_tracker.values())
+    return (points_total, total_copies)
 
 
 puzzle = Puzzle(2023, 4)
