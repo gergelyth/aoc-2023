050e465259d21586d83e66d52678987bb214c66f
gergelyth <gergelytothdev@gmail.com>
2023-12-06 05:39:20 +0000

    Day 6 - Part 2 done

--- day6/main.py
+++ day6/main.py
@@ -1,5 +1,4 @@
 from aocd.models import Puzzle
-from math import prod
 
 from core import test_and_submit
 from util import get_lines
@@ -9,32 +8,27 @@ class Race:
         self.time = time
         self.max_distance = distance
         
-    def get_possible_button_times(self) -> list[int]:
-        results = []
+    def get_possible_button_times(self) -> int:
+        possible_ways = 0
         for hold_button_time in range(self.time):
-            travel_time = self.time - hold_button_time
-            distance_traveled = hold_button_time * travel_time
+            distance_traveled = hold_button_time * (self.time - hold_button_time)
             if distance_traveled > self.max_distance:
-                results.append(hold_button_time)
+                possible_ways += 1
                 
-        return results
+        return possible_ways
         
-def parse_races(lines: list[str]) -> list[Race]:
-    races = []
+def parse_race(lines: list[str]) -> Race:
     times = list(filter(None, lines[0].split(" "))) 
     distances = list(filter(None, lines[1].split(" "))) 
-    for i in range(1, len(times)):
-        races.append(Race(int(times[i].strip()), int(distances[i].strip())))
-    return races
+    time = "".join(times[1:]).strip()
+    distance = "".join(distances[1:]).strip()
+    return Race(int(time), int(distance))
 
 def solution(input: str) -> tuple[any, any]:
     lines = get_lines(input)
-    races = parse_races(lines)
-    product = 1
-    for race in races:
-        result = len(race.get_possible_button_times())
-        product *= result
-    return (product, None)
+    race = parse_race(lines)
+    possible_ways = race.get_possible_button_times()
+    return (None, possible_ways)
 
 puzzle = Puzzle(2023, 6)
 test_and_submit(puzzle, solution, False)
